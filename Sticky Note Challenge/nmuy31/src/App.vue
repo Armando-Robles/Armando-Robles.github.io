eafac5436692dc4dc81dadec9cb8ada298a97992
Armando-Robles <93309435+Armando-Robles@users.noreply.github.com>
2022-08-08 23:49:27 -0600

    Add files via upload

---
+++ Sticky Note Challenge/nmuy31/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="nav"></div>
+  <router-view />
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background-color: rgb(53, 53, 53);
+}
+#app {
+  font-family: "Redressed";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #00b5bb;
+  height: 100vh;
+}
+</style>
